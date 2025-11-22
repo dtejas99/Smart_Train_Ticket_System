7e0896beda9caa10e78a31405365f7a3de267024
D Tejas <dtejas9980@gmail.com>
2025-11-22 21:50:17 +0530

    Update DatabaseConnection.java

--- src/main/java/com/train/DatabaseConnection.java
+++ src/main/java/com/train/DatabaseConnection.java
@@ -8,15 +8,15 @@ public class DatabaseConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/RAILWAY_SYSTEM";
     private static final String USER = "root";
-    private static final String PASSWORD = "root"; // change this
+    private static final String PASSWORD = "root"; 
 
     public static Connection getConnection() {
         Connection conn = null;
         try {
             conn = DriverManager.getConnection(URL, USER, PASSWORD);
-            System.out.println(" Database connected successfully!");
+            System.out.println("✅ Database connected successfully!");
         } catch (SQLException e) {
-            System.out.println(" Database connection failed: " + e.getMessage());
+            System.out.println("❌ Database connection failed: " + e.getMessage());
         }
         return conn;
     }
